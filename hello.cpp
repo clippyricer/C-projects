bc2e2a3c91b470d69f6345eb97f0100479f2e764
clippyricer <bannansmooth68@outlook.com>
2026-01-24 18:46:10 -0500

    cmake

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  std::cout << "Gentoo linux is the goat distro \n";
+  std::cout << "Gentoo penguins are also goated" << std::endl;
+  return 0;
+}
