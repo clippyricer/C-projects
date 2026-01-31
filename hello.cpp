4be86e3c77a8fb6af34a411960f76837166796e4
clippyricer <bannansmooth68@outlook.com>
2026-01-31 15:14:31 -0500

    "update"

--- hello.cpp
+++ hello.cpp
@@ -7,8 +7,13 @@ int main() {
   std::cout << a;
   std::cout << b << std::endl;
   std::cout << "6 + 7 is equal to " << sum << std::endl;
-  std::cout << "Hello, World!" << std::endl;
+  std::cout << "Hello, World!\n";
   std::cout << "Gentoo linux is the goat distro \n";
-  std::cout << "Gentoo penguins are also goated" << std::endl;
-  return 0;
+  std::cout << "Gentoo penguins are also goated\n";
+  
+  int age;
+  std::cout << "Enter your age: ";
+  std::cin >> age;
+  std::cout << "You are " << age << " years old\n";
+  return 0; 
 }
