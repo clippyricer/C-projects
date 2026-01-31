5bfe7865a03d2df8e795ee57606bf9fb7dc34c31
clippyricer <bannansmooth68@outlook.com>
2026-01-31 14:33:17 -0500

    add gitignore

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 int main() {
+  int a, b;
+  double c;
+  a = 6;
+  b = 7;
+  std::cout << a;
+  std::cout << b << std::endl;
   std::cout << "Hello, World!" << std::endl;
   std::cout << "Gentoo linux is the goat distro \n";
   std::cout << "Gentoo penguins are also goated" << std::endl;
