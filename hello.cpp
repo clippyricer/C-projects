a543556d447dee5ff0136f6654787939335a92c4
clippyricer <bannansmooth68@outlook.com>
2026-01-31 15:03:14 -0500

    ""

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
 int main() {
-  int a, b;
-  double c;
-  a = 6;
-  b = 7;
+  int a { 6 };
+  int b { 7 };
+  int sum = a + b;
   std::cout << a;
   std::cout << b << std::endl;
+  std::cout << "6 + 7 is equal to " << sum << std::endl;
   std::cout << "Hello, World!" << std::endl;
   std::cout << "Gentoo linux is the goat distro \n";
   std::cout << "Gentoo penguins are also goated" << std::endl;
